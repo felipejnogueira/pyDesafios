c28ad7cbc443fea54b059fcdf91fd316cbcb6d3c
felipejnogueira <fjnog1993@gmail.com>
2022-08-29 23:59:08 -0300

    Treino RankUp

--- CodeWars/RankUp.py
+++ CodeWars/RankUp.py
@@ -1,8 +1,15 @@
 # Funções para resolução dos Katas de nível RankUp
 
-def removeNumerosLista(lista = [], remover = []):
-    for itemLista in lista:
-        for itemRemove in remover:
-            if itemLista == itemRemove:
-                lista.pop(itemLista)
-    return lista
+# Retorna array sem os numeros contidos na segunda array
+def removeNumerosLista(listaOriginal, listaRemover):
+    return [x for x in listaOriginal if x not in listaRemover]
+
+
+# Constroi arvore com *
+def constroiArvore(andares):
+    return [("*" * (i * 2 - 1)).center(n * 2 - 1) for i in range(1, n + 1)]
+
+
+# Retorna ( se string so possui 1 ocorrencia da letra na palavra e ) se possui mais
+def retornaParenteses(word):
+    return ''.join('(' if word.casefold().count(word[i].casefold()) == 1 else ')' for i in range(0, len(word)))
--- CodeWars/TestRankUp.py
+++ CodeWars/TestRankUp.py
@@ -1,3 +1,6 @@
 from RankUp import *
 
-print(removeNumerosLista([1, 2, 2, 3], [2]))
+#print(removeNumerosLista([1, 2, 3, 5, 2, 10, 2, 2], [1, 2]))
+
+#print(constroiArvore(4))
+print(retornaParenteses('abelha'))
